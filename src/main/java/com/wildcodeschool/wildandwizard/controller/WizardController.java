758ff0123979ecefc04e551d2b3f0b7d6cb5eac4
Adrien Souquere <adriensouquere@gmail.com>
2019-11-18 10:16:08 +0100

    deuxième essai

--- src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
@@ -6,14 +6,13 @@ import com.wildcodeschool.wildandwizard.repository.WizardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Service 
+ 
 @Controller
 public class WizardController {
 	
